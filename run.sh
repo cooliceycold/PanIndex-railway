b8232f30d5d331373f6512e18aa47196b2930ee4
cooliceycold <112804160+cooliceycold@users.noreply.github.com>
2025-03-07 10:14:03 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
-version="$PAN_INDEX_VERSION"
-if [ "$version" = "" ]
-then
-    version=`curl --silent "https://api.github.com/repos/libsgh/PanIndex/releases/latest" \
-        | grep '"tag_name":' \
-        | sed -E 's/.*"([^"]+)".*/\1/'`
-fi
-curl -sOL "https://github.com/libsgh/PanIndex/releases/download/${version}/PanIndex-linux-amd64.tar.gz"
+
+curl -sOL "https://github.com/px-org/PanIndex/releases/download/v3.1.5/PanIndex-linux-amd64.tar.gz"
 md5sum "PanIndex-linux-amd64.tar.gz"
 tar -zxvf "PanIndex-linux-amd64.tar.gz"
 rm -rf README.md LICENSE
